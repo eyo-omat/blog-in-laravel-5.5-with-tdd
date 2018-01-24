a6db8b8a95323924f6e911021ca450e31740710d
Eyioyo <eyo.omatgmail.com>
2018-01-24 16:33:16 +0000

    Added test for comments, added user and date to comments

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -15,12 +15,14 @@
             </div>
         </div>
     </div>
-    </div>
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             @foreach ($post->comment as $comment)
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{ $comment->created_at->diffForHumans() }}</div>
+                    <div class="panel-heading">
+                        {{ $comment->creator->name }} comment since
+                        {{ $comment->created_at->diffForHumans() }}
+                    </div>
                         <div class="panel-body">
                             <article>
                                 <div class="body"> {{ $comment->body }} </div>
@@ -30,4 +32,5 @@
             @endforeach
         </div>
     </div>
+</div>
 @endsection
